523c296bf056bd5193e621f92bf2dc67d9c464c3
Suraj Kushwah <6266988978sksingh@gmail.com>
2026-03-16 16:35:45 +0530

    Update axiosInstance.js

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://reviewdesk.onrender.com",
+  baseURL: "https://reviewdesk.onrender.com/api",
 });
 
 axiosInstance.interceptors.request.use((config) => {
